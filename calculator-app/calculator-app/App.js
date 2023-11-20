7a177d7031316398567c6a7413071c7693ee4c8a
0947438453 <kha16112004@gmail.com>
2023-11-20 16:25:23 +0700

    Capnhap-BT3

--- calculator-app/calculator-app/App.js
+++ calculator-app/calculator-app/App.js
@@ -12,12 +12,38 @@ export const BMI = () => {
   const [weight, setWeight] = useState('');
   const [height, setHeight] = useState('');
   const [BMI, setBMI] = useState(0);
+  const [ktBMI, setKtBMI] = useState('');
+
+  const KTBMI = (bmivaule) => {
+    if (bmivaule > 40) {
+      return 'Béo phì độ III';
+    } else if (35 < bmivaule && bmivaule < 40) {
+      return 'Béo phì độ II';
+    } else if (30 < bmivaule && bmivaule < 35) {
+      return 'Béo phì độ I';
+    }
+    else if (25 < bmivaule && bmivaule < 30) {
+      return 'Tiền béo phì';
+    }
+    else if (18.5 < bmivaule && bmivaule < 25) {
+      return 'Bình thường';
+    }else if (bmivaule <18.5) {
+      return 'Cân nặng thấp';
+    }
+  };
 
   const computeBMI = () => {
     const weightInKg = parseFloat(weight);
     const heightInM = parseFloat(height) / 100;
     const bmi = weightInKg / (heightInM * heightInM);
     setBMI(bmi);
+    setKtBMI(KTBMI(bmi));
+  };
+  const clean = () =>{
+    setWeight ('');
+    setHeight ('');
+    setBMI (0);
+    setKtBMI('');
   };
 
   return (
@@ -28,7 +54,7 @@ export const BMI = () => {
           style={styles.input}
           value={weight}
           onChangeText={(text) => setWeight(text)}
-          keyboardType="numeric" 
+          keyboardType="numeric"
         />
       </View>
       <View style={styles.inputContainer}>
@@ -40,57 +66,78 @@ export const BMI = () => {
           keyboardType="numeric"
         />
       </View>
+
       <Text style={styles.result}>BMI: {BMI.toFixed(2)}</Text>
-      <TouchableOpacity style={styles.button} onPress={computeBMI}>
+      <Text style={styles.result}>Tình trạng: {ktBMI}</Text>
+
+      <TouchableOpacity style={styles.button1} onPress={computeBMI}>
         <Text style={styles.buttonText}>Tính toán</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button2} onPress={clean}>
+        <Text style={styles.buttonText}>Làm mới</Text>
       </TouchableOpacity>
     </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 20,
-  },
-  inputContainer: {
-    width: '80%',
-    alignItems: 'flex-start',
-  },
-  label: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginVertical: 10,
-  },
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    padding: 10,
-  },
-  button: {
-    backgroundColor: 'lightblue',
-    width: '40%',
-    height: 50,
-    marginTop: 20,
-    color: '#fff',
-    borderWidth: 2,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  result: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginVertical: 10,
-  },
-});
+const styles = StyleSheet.create({   
+  container: {     
+    flexGrow: 1,     
+    backgroundColor: '#fff',     
+    alignItems: 'center',     
+    justifyContent: 'center',     
+    paddingVertical: 20,   
+    },   
+    inputContainer: {     
+      width: '80%',     
+      alignItems: 'flex-start',   
+    },   
+    label: {     
+      fontSize: 16,     
+      fontWeight: 'bold',     
+      marginVertical: 10,   
+    },   
+    input: {     
+      width: '100%',    
+      height: 40,    
+      borderColor: '#ccc',     
+      borderWidth: 1,     
+      padding: 10,   
+    },   
+    button1: {     
+      backgroundColor: 'lightgreen',     
+      width: '40%',     
+      height: 50,     
+      marginTop: 20,
+      borderRadius: 20,
+      color: '#fff',   
+      left: -90,  
+      borderWidth: 2,
+      alignItems: 'center',    
+      justifyContent: 'center',   
+    },   
+    button2: {     
+      backgroundColor: 'red',     
+      width: '40%',     
+      height: 50,     
+      marginTop: -50,
+      borderRadius: 20,
+      left: 90,
+      color: '#fff',     
+      borderWidth: 2,
+      alignItems: 'center',    
+      justifyContent: 'center',   
+    },   
+    buttonText: {     
+      fontSize: 16,    
+      fontWeight: 'bold',   
+    },   
+    result: {     
+      fontSize: 16,     
+      fontWeight: 'bold',     
+      marginVertical: 10,   
+    }, 
+  });
 
 export default BMI;
